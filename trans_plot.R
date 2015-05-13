9731c74eba32bbb7c2541d11e68d0891a7f5b52c
tadzi <tadzio@d-a140-215.external.eduroam.uq.edu.au>
2015-05-13 11:32:26 +1000

    find centroid per transect
    get coordinates of photo quadrant halfway of transect. Plot centroid
    using ggmap.
    
    Turns out that there is high variability in number of images per
    transects. -> Manuel checks database

--- trans_plot.R
+++ trans_plot.R
@@ -1,1 +1,26 @@
-cord = aggregate(cbind(V4,V5)~V3, data=tdata, mean)
+#import data file
+data = read.csv(file.choose(),head=FALSE)
+colnames(data) = c("id","image","trans","lat","lon","label","func. group","descr.","cover")
+
+#cast dataset
+cdata = cast(id+image+trans+lat+lon~label,data=data,mean)
+
+#calculate centroid per transect
+trans = unique(cdata$trans)
+im.count = data.frame()
+
+
+for (i in 1:length(trans))
+{
+  im.count[i,1] = trans[i]
+  im.count[i,2] = length(unique(cdata[cdata$trans == trans[i],2]))
+  im.count[i,3] = unique(cdata[cdata$trans == trans[i],1])[round(im.count[i,2]/2)]
+  im.count[i,4] = cdata[cdata$id==im.count[i,3],4]
+  im.count[i,5] = cdata[cdata$id==im.count[i,3],5]
+}
+
+
+
+map = get_map(location = c(lon = 145.8981, lat = -15), zoom=5, scale = "auto", maptype = "hybrid")
+ptrans = ggmap(map) + geom_point(aes(x=V5, y=V4, colour=factor(V1)), data=im.count)
+ptrans 
