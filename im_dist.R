f7e41ee81f918d1ad8c13a401b50e2e32010c236
tadzi <tadzio@d-a140-200.external.eduroam.uq.edu.au>
2015-05-18 18:47:23 +1000

    aggregate by images
    added aggregate function to select unique images and calculate distance
    between them.

--- im_dist.R
+++ im_dist.R
@@ -1,9 +1,13 @@
-#declare variable for storage histograms per transect. atm not sure if data.frame is appropriate data struct to store histograms
+# declare variable for storage histograms per transect. atm not sure if data.frame is appropriate data struct to store histograms
 imdist = data.frame()
 
-#subset the dataset per transect and calculate distance between images using dist function
+# subset the dataset per transect and calculate distance between images using dist function
 for (i in 1:length(unique(cdata$trans)))
 {
-  sdata = subset(cdata,trans==unique(trans)[i])
+  # subset the dataset by transect and aggregate subset by unique images; aggregate prevents calculation of distances
+  # between coordinates of the same image
+  
+  sdata = aggregate(cbind(lat,lon)~image,data = subset(cdata,trans==unique(trans)[i]), mean)
+  
   imdist[i] = hist(dist(sdata$lat,sdata$lon)) 
 }
