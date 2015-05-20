8598f4b1634a52618f60c1b647ac4d9605cb81c5
tadzi <tadzio@d-a140-195.external.eduroam.uq.edu.au>
2015-05-20 14:26:39 +1000

    exclude transects with invalid coordinates
    - excluding transects with weird coordinates.
    - new bins created
    - to do: retrieve min and max distance per transect for correlogram
    analysis

--- im_dist.R
+++ im_dist.R
@@ -1,23 +1,24 @@
-#set number of bins and breaks
-bins = 500
-breaks = seq(0,bins)
+#set breaks
+breaks = c(0:10,50,100,1000,10000)
 
 #retrieve unique transect numbers
 untrans = unique(cdata$trans)
 #remove transects with invalid coordinate values
-untrans = untrans[!untrans %in% c(10007,15011)]
+untrans = untrans[!untrans %in% c(10004,10005,10007,10009,14008,15011)]
 
 #declare data frame for frequency per bin per transect
-imdist = data.frame(matrix(NA, nrow=length(untrans), ncol=bins+1))
-
+imdist = data.frame(matrix(NA, nrow=length(untrans), ncol=length(breaks)))
 
 # subset the dataset per transect and calculate distance between images using dist function
 for (i in 1:length(untrans))
 {
   # subset the dataset by transect and aggregate subset by unique images; aggregate prevents calculation of distances
   # between coordinates of the same image  
+  sdata = data.frame()
   sdata = aggregate(cbind(lat,lon)~image,data = subset(cdata,trans==untrans[i]), mean)
   
   #per transect, calculate distance between images and determine frequency using bins defined by breaks
-  imdist[i,] = cbind(untrans[i], t(hist(dist(sdata)$dist,breaks=breaks)$counts))
+  idist = dist(sdata)
+  imdist[i,] = cbind(untrans[i], t(hist(idist$dist,breaks=breaks)$counts))
 }
+
