993708b213f6e9f4c5d18caf3281842828799d51
tadzi <tadzio@d-a140-195.external.eduroam.uq.edu.au>
2015-05-20 14:27:51 +1000

    forgot brackets :)

--- dist.R
+++ dist.R
@@ -8,6 +8,7 @@ dist = function(dat)
   
   #declare output as vector
   len = length(dat[,1])
+  outl = list()
   d = data.frame(image = numeric(len),
                  dist = numeric(len))
   
@@ -18,10 +19,16 @@ dist = function(dat)
   as(tdata,"SpatialPoints")
   
   # iterate over the utm coordinates and calculate distance using pythagoras
-  for (i in 1:len-1)
+  for (i in 1:(len-1))
   {
     d[i,1] = tdata$image[i]
     d[i,2] = sqrt((tdata$lat[i+1]-tdata$lat[i])^2+(tdata$lon[i+1]-tdata$lon[i])^2)
+    
+    if (d[i,2]>100)
+    {
+      outl[length(outl)+1] = d[i,1]
+    }
+    
   }
   
   return(d)
