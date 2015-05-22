f129357e9766574b90ddc9f4ce489f942117910a
tadzi <tadzio@d-a140-139.external.eduroam.uq.edu.au>
2015-05-22 15:43:48 +1000

    corrected mistake
    exclude images>2529 from transect 14012 instead of selecting them

--- import_dat.R
+++ import_dat.R
@@ -7,4 +7,5 @@ cdata = cast(id+image+trans+lat+lon~label,data=data,mean)
 
 # remove datapoints from transect 14012 where image # > 2529 (these images don't belong to transect)
 # transects 10004, 10005, 10007, 15011, 14008, 10009 are excluded in im_distr.R script
-cdata =cdata[cdata$trans==14012 & cdata$image>140122529,]
+cdata =cdata[!(cdata$trans==14012 & cdata$image>140122529),]
+rownames(cdata) = NULL
