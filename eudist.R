417edab03f2726915c954f83e3ea06c7e0de2c85
tadzi <tadzio@d-a141-220.external.eduroam.uq.edu.au>
2015-06-30 17:24:09 +1000

    name change function
    dist already exists in {stats}

--- eudist.R
+++ eudist.R
@@ -3,36 +3,30 @@
 # function calculates euclidian distance between coordinates. Estimates should be ok for short distances between coordinates.
 # function takes geographical coodinates and projects these to UTMs
 # function does not assume curvature of the earth
+# function needs "lon" and "lat" columns
 #****************************************************************************************************************************
 
-dist = function(dat)
+eudist = function(dat)
 {
   library("sp")
   library("rgdal")
   
-  #declare output as vector
+  # declare output as df
   len = length(dat[,1])
-  outl = list()
   d = data.frame(image = numeric(len),
                  dist = numeric(len))
   
-  #project coordinates to utm
+  # project coordinates to utm
   coordinates(dat) = c("lon","lat")
   proj4string(dat) = CRS("+proj=longlat +datum=WGS84")
-  tdata<-spTransform(dat,CRS("+proj=utm +zone=55 +datum=WGS84"))
+  tdata = spTransform(dat,CRS("+proj=utm +zone=55 +datum=WGS84"))
   as(tdata,"SpatialPoints")
   
-  # iterate over the utm coordinates and calculate distance using pythagoras
+  # iterate over the utm coordinates and calculate euclidian distance
   for (i in 1:(len-1))
   {
     d[i,1] = tdata$image[i]
     d[i,2] = sqrt((tdata$lat[i+1]-tdata$lat[i])^2+(tdata$lon[i+1]-tdata$lon[i])^2)
-    
-    if (d[i,2]>100)
-    {
-      outl[length(outl)+1] = d[i,1]
-    }
-    
   }
   
   return(d)
