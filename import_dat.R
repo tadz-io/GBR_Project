33b7941a6ea1278ddf08764f16091ec1761fa826
tadzi <tadzio@d-a140-193.external.eduroam.uq.edu.au>
2015-07-07 12:33:26 +1000

    excluding additional transects
    additional transects to exclude. See read

--- import_dat.R
+++ import_dat.R
@@ -14,7 +14,7 @@ cdata = cast(id+image+trans+lat+lon~label,data=data,mean)
 # remove datapoints from transect 14012 where image # > 2529 (these images don't belong to transect)
 # remove transects 10004, 10005, 10007, 10009, 14008, 15011 (see comments logbook)
 cdata =cdata[!(cdata$trans==14012 & cdata$image>140122529),]
-cdata = subset(cdata, !cdata$trans %in% c(10004,10005,10007,10009,14008,14014,15011))
+cdata = subset(cdata, !cdata$trans %in% c(10004,10005,10007,15011,14008,10009,14014,11005,12003,12008,12024,12029,12031,14014,15018))
 rownames(cdata) = NULL
 
 # count number of quadrats per image
