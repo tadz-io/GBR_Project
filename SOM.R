fbc6a601ab79f9b60507d041e15a39ee8bf70dc6
tadzi <tadzio@d-a140-107.external.eduroam.uq.edu.au>
2015-07-08 17:08:07 +1000

    normalising trainingset
    training set is normalised per functional group

--- SOM.R
+++ SOM.R
@@ -1,16 +1,24 @@
 # Kohonen SOM
 library("kohonen")
+library("reshape")
+library("ggplot2")
 
 # create training set
+# ---------------------------------------------------------------------
 # use clustered dataset (cldata): trans_cluster.R
-train = as.matrix(cldata[,2:22])
-# check if scaling is still required (see Park et al. 2004)
+# normalizing density data per functional group (see Park et al. 2004)
+train.n = apply(cldata[,2:22], 2, function(x){
+  return((x - min(x))/(max(x) - min(x)))
+  })
 
 # initialize SOM grid
 som.grid = somgrid(xdim = 4, ydim = 4, topo = "hexagonal")
 
+
+
 # start training of SOM
-mod = som(train,
+# ----------------------
+mod = som(train.n,
           grid = som.grid,
           rlen = 100,
           alpha = c(.05, .01),
@@ -27,10 +35,6 @@ plot(mod, type = "changes")
 plot(mod, type = "counts")
 
 # plot composition of SOM nodes
-library("reshape")
-library("ggplot2")
-library("RColorBrewer")
-
 ncomp = melt(t(mod$codes))
 colnames(ncomp) = c("fgroup", "node", "cover")
 ggplot(ncomp, aes(x = node, y = cover, fill=fgroup)) +
