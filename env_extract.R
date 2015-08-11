f117f5dd0438b3da6e83b9f520a8126b1957831a
tadzi <tadzio@d-a141-198.external.eduroam.uq.edu.au>
2015-08-11 17:35:59 +1000

    Data extraction GIS
    script extracts environmental variables from GIS raster files.

---
+++ env_extract.R
@@ -0,0 +1,15 @@
+library("rgdal")
+library("raster")
+library("sp")
+
+# import environmental data as raster
+cots = raster(file.choose())
+cyclo = raster(file.choose())
+
+# set coordinates of sampling locations
+samp = data.frame(image = cdata$image, lon = cdata$lon, lat = cdata$lat)
+coordinates(samp) = c("lon", "lat")
+
+# extract environmental variables for every sampling location
+cyclo.ex = data.frame(extract(cyclo, samp, method="simple", sp = T)@data)
+cots.ex = data.frame(extract(cots, samp, method="simple", sp = T)@data)
