c85a192746cc1744d809d66270b8e75205d1d4e1
tadzi <tadzio@d-a141-168.external.eduroam.uq.edu.au>
2015-09-15 11:08:34 +1000

    extract environmental vars
    script to extract environmental variables from grid files

--- env_extract.R
+++ env_extract.R
@@ -13,3 +13,9 @@ coordinates(samp) = c("lon", "lat")
 # extract environmental variables for every sampling location
 cyclo.ex = data.frame(extract(cyclo, samp, method="simple", sp = T)@data)
 cots.ex = data.frame(extract(cots, samp, method="simple", sp = T)@data)
+
+colnames(cyclo.ex) = c("image", "cyclo")
+colnames(cots.ex) = c("image", "cots")
+
+# create df with environmental variables
+env = data.frame(image=cyclo.ex$image, cyclo = cyclo.ex$cyclo, cots = cots.ex$cots)
