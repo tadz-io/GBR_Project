be89fdfbc18e4a0016faf183b7c4f60687d2abf1
tadzi <tadzio@d-a141-80.external.eduroam.uq.edu.au>
2015-10-15 16:18:03 +1000

    reprojection
    now includes lines to reproject the original raster to an extended
    raster

--- cyclo_metrics.R
+++ cyclo_metrics.R
@@ -8,18 +8,23 @@ len = length(rfiles)
 ext = data.frame(xmin = rep(NA, len),
                  xmax = rep(NA, len),
                  ymin = rep(NA, len),
-                 ymax = rep(NA, len))
+                 ymax = rep(NA, len),
+                 xres = rep(NA, len),
+                 yres = rep(NA, len))
 
 # determine extent necessary to create stack
 for(i in 1:len){
   # load raster
   rast = raster(rfiles[i])
-  projectRaster(rast, crs = CRS("+proj=longlat +datum=WGS84"))
+  # reproject raster
+  rast = projectRaster(rast, crs = CRS("+proj=longlat +datum=WGS84"))
   # fill ext df
   ext$xmin[i] = extent(rast)@xmin
   ext$xmax[i] = extent(rast)@xmax
   ext$ymin[i] = extent(rast)@ymin
   ext$ymax[i] = extent(rast)@ymax
+  ext$xres[i] = res(rast)[1]
+  ext$yres[i] = res(rast)[2]
 }
 
 # create extent object that encompasses all rasters
@@ -30,13 +35,20 @@ area = extent(c(min(ext$xmin),
                 max(ext$ymax)))
 # init stack
 cyclStack = stack()
+# init projection raster
+r.proj = raster(area, crs = crs(rast))
+# set resolution
+res(r.proj) = res(rast)
 
 # iteratively load raster files into a stack
 for(i in 1:len){
-  browser()
+  #load raster files again
   rast = raster(rfiles[i])
-  # extend raster files
-  rast = extend(rast, area, value = NA)
+  # reproject again
+  rast = projectRaster(rast, crs = CRS("+proj=longlat +datum=WGS84"))
+
+  # reproject raster on extended raster file using nearest neigbor interpolation
+  rast = resample(rast, r.proj, method = "ngb")
   # add layer to stack
   cyclStack = addLayer(cyclStack, rast)
 }
